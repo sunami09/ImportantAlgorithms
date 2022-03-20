b6150eb2851f722f7af4ad40e163926c5e2aab06
sunami09 <sunamidasgupta@gmail.com>
2022-03-20 21:04:17 +0530

    Selection Sort

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -18,6 +18,24 @@ void SelectionSortV(vector<int> &arr)
     }
 }
 
+void SelectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 int main()
 {
     vector<int> arr = {7, 4, 2, 1, 7, 5, 0};
