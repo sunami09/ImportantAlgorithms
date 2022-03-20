367bad0fe1abde0695f7761314dc7f9b789b8a61
sunami09 <sunamidasgupta@gmail.com>
2022-03-20 20:54:49 +0530

    Selection Sort

---
+++ SelectionSort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void SelectionSortV(vector<int> &arr)
+{
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[min]){
+                min = j;
+            }
+        }
+        int temp = arr[min];
+        arr[min] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+int main()
+{
+    vector<int> arr = {7, 4, 2, 1, 7, 5, 0};
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    SelectionSortV(arr);
+    cout << endl;
+    for (int i = 0; i < 7; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
